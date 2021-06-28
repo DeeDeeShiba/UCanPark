457c6f3a812ed9d534cab10ab59b853c69f7f69a
David Romero <a.davidromero@icloud.com>
2021-06-28 03:49:27 -0400

    Completed Geometry.dart

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,11 +1,12 @@
-class Location{
-
+class Location {
   late final double lat;
   late final double lng;
+
   Location({required this.lat, required this.lng});
 
   //this is to map properties from the JSON object to the DART object
 
-  Location.fromJson(Map<dynamic, dynamic> parsedJson): lat = parsedJson['lat'],
-  lng = parsedJson['lng'];
+  Location.fromJson(Map<dynamic, dynamic> parsedJson)
+      : lat = parsedJson['lat'],
+        lng = parsedJson['lng'];
 }
