0b4378b8c987b7d9ff9fbe2b968f3852c07df0c9
David Romero <a.davidromero@icloud.com>
2021-06-28 04:07:28 -0400

    completed place_of_operation.dart

---
+++ lib/models/place_of_operation.dart
@@ -0,0 +1,19 @@
+import 'package:u_can_park/models/geometry.dart';
+
+class PlaceOfOperation {
+  late final String name;
+  late final double rating;
+  late final Geometry geometry;
+  late final String nearBy;
+
+  PlaceOfOperation(
+      {required this.geometry, required this.name, required this.rating, required this.nearBy});
+
+  PlaceOfOperation.fromJson(Map<dynamic, dynamic> parsedJson)
+      :name = parsedJson['name'],
+        rating = (parsedJson ['rating'] != null) ? parsedJson['rating']
+            .toDouble() : null,
+        nearBy = parsedJson['nearBy'],
+  geometry = Geometry.fromJson(parsedJson['geometry']);
+
+}
